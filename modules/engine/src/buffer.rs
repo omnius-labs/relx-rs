7c9dc505e4ed8462b61a9d8bb7a21f957ea195f0
Lyrise <lyrise1984@gmail.com>
2023-12-16 10:51:10 +0900

    fix

--- modules/engine/src/buffer.rs
+++ modules/engine/src/buffer.rs
@@ -139,4 +139,37 @@ impl BufferPoolManager {
         self.page_table.insert(page_id, buffer_id);
         Ok(page)
     }
+
+    pub fn create_page(&mut self) -> Result<Rc<Buffer>, Error> {
+        let buffer_id = self.pool.evict().ok_or(Error::NoFreeBuffer)?;
+        let frame = &mut self.pool[buffer_id];
+        let evict_page_id = frame.buffer.page_id;
+        let page_id = {
+            let buffer = Rc::get_mut(&mut frame.buffer).unwrap();
+            if buffer.is_dirty.get() {
+                self.disk.write_page_data(evict_page_id, buffer.page.get_mut())?;
+            }
+            let page_id = self.disk.allocate_page();
+            *buffer = Buffer::default();
+            buffer.page_id = page_id;
+            buffer.is_dirty.set(true);
+            frame.usage_count = 1;
+            page_id
+        };
+        let page = Rc::clone(&frame.buffer);
+        self.page_table.remove(&evict_page_id);
+        self.page_table.insert(page_id, buffer_id);
+        Ok(page)
+    }
+
+    pub fn flush(&mut self) -> Result<(), Error> {
+        for (&page_id, &buffer_id) in self.page_table.iter() {
+            let frame = &self.pool[buffer_id];
+            let mut page = frame.buffer.page.borrow_mut();
+            self.disk.write_page_data(page_id, page.as_mut())?;
+            frame.buffer.is_dirty.set(false);
+        }
+        self.disk.sync()?;
+        Ok(())
+    }
 }
